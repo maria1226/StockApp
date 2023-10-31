30beafc8e22f6c69025efc9406fcd660cda09641
maria1226 <maria.c.rakova@gmail.com>
2023-10-31 12:29:59 +0200

    refetchstocks

--- StockApp-FE/pages/index.tsx
+++ StockApp-FE/pages/index.tsx
@@ -14,27 +14,42 @@ const Home: React.FC = () => {
   const [stockData, setStockData] = useState<StockDataItem[]>([]);
 
   useEffect(() => {
-    // Fetch data when the component mounts
+    // Fetch data when the component mounts (initial load)
+    fetchStockData();
+  }, []);
+
+  const handleDateSelect = (dates: { start: string, end: string }): void => {
+    // Fetch data again when dates are selected
+    fetchStockData(dates.start, dates.end);
+  };
+
+  const fetchStockData = (startDate?: string, endDate?: string) => {
+    const query = {
+      query: `
+        query Stocks($startDate: String, $endDate: String) {
+          stocks(startDate: $startDate, endDate: $endDate) {
+            stocks {
+              price
+              date
+            }
+          }
+        }
+      `,
+      variables: {
+        startDate: startDate || null,
+        endDate: endDate || null,
+      },
+    };
+
     fetch("http://localhost:4000/graphql", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `
-          query Stocks {
-            stocks {
-              stocks {
-                price
-                date
-              }
-            }
-          }
-        `,
-      }),
+      body: JSON.stringify(query),
     })
       .then((response) => response.json())
       .then((data) => setStockData(data.data.stocks.stocks))
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  };
 
   return (
     <Layout>
@@ -43,9 +58,7 @@ const Home: React.FC = () => {
       </Head>
       <div className="research">Analyze Apple Stock Performance Over Time</div>
       <div className={styles["custom-chart-container"]}>
-        <StockGraph data={stockData} onDateSelect={function (selectedDates: { start: string; end: string; }): void {
-          throw new Error("Function not implemented.");
-        } } />
+        <StockGraph data={stockData} onDateSelect={handleDateSelect} />
       </div>
     </Layout>
   );
